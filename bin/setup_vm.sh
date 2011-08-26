a045b0dfaf0170c20f86c219dba904d85318dd0b
Stephen Woodrow <srwoodrow@gmail.com>
2011-08-26 14:17:33 -0400

    Update docs.

--- bin/setup_vm.sh
+++ bin/setup_vm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #-------------------------------------------------------------------------------
-# This script should set up a vanilla Debian VM installed from a CD image to
-# allow it to be used for bismark-simulcast.
+# This script should set up a vanilla Ubuntu Server VM installed from a CD
+# image to allow it to be used for bismark-simulcast.
 #
 # This script must be run as root.
 #
@@ -12,20 +12,9 @@
 # Speaking of virtual machines, this has been tested out on a VirtualBox OSE vm
 # configured with the following interfaces:
 #
-#   eth0: NAT-ed to host OS (for Internet access)
-#   eth1: bridged to host's vboxnet0 (host-only, for SSH)
-#   eth2: bridged to host's ethernet port
+#   eth0: NAT-ed to host OS (for Internet access) eth1: bridged to host's
+#   vboxnet0 (host-only, for SSH) eth2: bridged to host's ethernet port
 #-------------------------------------------------------------------------------
-
-# add main repos to sources.list
-echo "deb http://ftp.us.debian.org/debian squeeze main" >> \
-    /etc/apt/sources.list
-echo "deb http://ftp.us.debian.org/debian/ squeeze-updates main" >> \
-    /etc/apt/sources.list
-
-# install packages
-apt-get install vlan
-apt-get install ifmetric
 
 # set up vbox interfaces
 ## eth0 is NAT-ed to host OS for Internet
@@ -37,3 +26,8 @@ echo "auto eth1" >> /etc/network/interfaces
 echo "iface eth1 inet dhcp" >> /etc/network/interfaces
 echo "    metric 20" >> /etc/network/interfaces
 ## eth2 is bridged to the host's ethernet port
+
+## install packages
+#apt-get install vlan
+#apt-get install ifmetric
+#apt-get install fping
